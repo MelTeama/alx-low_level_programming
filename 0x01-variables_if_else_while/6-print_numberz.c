9d0ef7f5413a19d3f614eb781e898a6203b8cb4a
MelTeama <melekte123@gmail.com>
2022-07-03 10:33:38 -0700

    putchar method of displaying single digit number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function parameter control
+ * Return: always 0
+ */
+
+int main(void)
+{
+		int n = 0;
+
+		for (; n < 10; n++)
+		{
+			putchar('0' + n);
+		}
+			putchar('\n');
+
+			return (0);
+}
