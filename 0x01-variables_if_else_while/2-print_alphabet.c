d0db97a01c8a3024d76ce4b30d59c8b9c1bf7968
MelTeama <melekte123@gmail.com>
2022-07-02 18:57:51 -0700

    display small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - character display
+ * Return: always 0
+ */
+
+		int main(void)
+{
+		int chr;
+
+		for (chr = 97; chr <= 122; chr++)
+
+
+{
+			putchar(chr);
+}
+			putchar(10);
+
+		return (0);
+}
