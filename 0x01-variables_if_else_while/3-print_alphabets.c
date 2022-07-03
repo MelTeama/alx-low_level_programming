34a99d0c73477110330736463d3ca7d0d0d3baab
MelTeama <melekte123@gmail.com>
2022-07-02 19:23:40 -0700

    small and capital letter displaying

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - character display
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int chr;
+
+	for (chr = 97; chr <= 122; chr++)
+
+	{
+		putchar(chr);
+	}
+	for (chr = 65; chr <= 96; chr++)
+	{
+		putchar(chr);
+	}
+		putchar(10);
+
+		return (0);
+}
