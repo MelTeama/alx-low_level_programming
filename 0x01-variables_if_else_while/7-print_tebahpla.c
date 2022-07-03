b287c85832fcd789707159b9caa2baec8a4ccc75
MelTeama <melekte123@gmail.com>
2022-07-03 10:50:40 -0700

    reversed letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function parameter control
+ * Return: always 0
+ */
+
+int main(void)
+{
+		int chr;
+
+		for (chr = 122; chr >= 97; chr--)
+		{
+			putchar(chr);
+		}
+			putchar('\n');
+
+			return (0);
+}
