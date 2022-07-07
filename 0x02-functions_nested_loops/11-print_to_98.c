55cec5af2ff3fab9d8e04222b3ea55bedeaf4063
MelTeama <melekte123@gmail.com>
2022-07-06 19:38:16 -0700

    number zero to ninetyeight

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - numbers from zero to ninetyeight
+ * @n: number integer
+ */
+
+void print_to_98(int n)
+
+{
+	for (n = 0; n < 98; n++)
+
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+
+	_putchar(98);
+	_putchar('\n');
+}
