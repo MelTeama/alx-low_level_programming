cdd3858ea5b1c268ff32678a2c8b3e5cf3f9a566
MelTeama <melekte123@gmail.com>
2022-07-06 19:41:09 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,14 +8,12 @@
 void print_to_98(int n)
 
 {
-	for (n = 0; n < 98; n++)
+	for (n = 0; n <= 98; n++)
 
 	{
 		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
 	}
-
-	_putchar(98);
 	_putchar('\n');
 }
